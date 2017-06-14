0cc33de6c75b7ffd97f09e48e87cbd149b72e44b
jcanabarro <joaovictorcana@gmail.com>
2017-06-14 09:32:33 -0300

    meus movimentos

--- cube.cpp
+++ cube.cpp
@@ -25,7 +25,7 @@ R L U D F B Mh Mv         -> Horarios
 R' L' U' D' F' B' Mh' Mv' -> Anti-Horario
 Total=16 movimentos
 */
-
+/*
 -------------
 R
 
@@ -169,6 +169,234 @@ top    bottom
 5 3 3  0 2 2
 5 3 3  0 2 2
 -------------
+
+-------------
+D
+
+front  back
+5 5 5  0 0 0
+5 5 5  0 0 0
+5 5 5  0 0 0
+
+left   right
+4 4 4  1 1 1
+4 4 4  1 1 1
+4 4 4  1 1 1
+
+top    bottom
+3 3 3  A B C
+3 3 3  H 2 D
+3 3 3  G F E
+
+->
+
+front  back
+5 5 5  0 0 0
+5 5 5  0 0 0
+4 4 4  1 1 1
+
+left   right
+4 4 4  1 1 1
+4 4 4  1 1 1
+0 0 0  5 5 5
+
+top    bottom
+3 3 3  G H A
+3 3 3  F 2 B
+3 3 3  E D C
+-------------
+
+-------------
+D'
+
+front  back
+5 5 5  0 0 0
+5 5 5  0 0 0
+5 5 5  0 0 0
+
+left   right
+4 4 4  1 1 1
+4 4 4  1 1 1
+4 4 4  1 1 1
+
+top    bottom
+3 3 3  A B C
+3 3 3  H 2 D
+3 3 3  G F E
+
+->
+
+front  back
+5 5 5  0 0 0
+5 5 5  0 0 0
+1 1 1  4 4 4
+
+left   right
+4 4 4  1 1 1
+4 4 4  1 1 1
+5 5 5  0 0 0
+
+top    bottom
+3 3 3  C D E  
+3 3 3  B 2 F
+3 3 3  A H G
+-------------
+
+-------------
+B
+
+front  back
+5 5 5  0 0 0
+5 5 5  0 0 0
+5 5 5  0 0 0
+
+left   right
+4 4 4  1 1 1
+4 4 4  1 1 1
+4 4 4  1 1 1
+
+top    bottom
+3 3 3  A B C
+3 3 3  H 2 D
+3 3 3  G F E
+
+->
+
+front  back
+5 5 5  C D E
+5 5 5  B 0 F
+5 5 5  A H G
+
+left   right
+4 4 4  1 1 1
+4 4 4  1 1 1
+3 3 3  2 2 2
+
+top    bottom
+3 3 3  2 2 2  
+3 3 3  2 2 2
+1 1 1  4 4 4
+-------------
+B'
+
+front  back
+5 5 5  0 0 0
+5 5 5  0 0 0
+5 5 5  0 0 0
+
+left   right
+4 4 4  1 1 1
+4 4 4  1 1 1
+4 4 4  1 1 1
+
+top    bottom
+3 3 3  A B C
+3 3 3  H 2 D
+3 3 3  G F E
+
+->
+
+front  back
+5 5 5  G H A 
+5 5 5  F 0 B
+5 5 5  E D C
+
+left   right
+4 4 4  1 1 1
+4 4 4  1 1 1
+2 2 2  3 3 3
+
+top    bottom
+3 3 3  2 2 2  
+3 3 3  2 2 2
+4 4 4  1 1 1
+-------------
+Mv
+
+front  back
+5 A 5  0 G 0
+5 B 5  0 H 0
+5 C 5  0 I 0
+
+left   right
+4 4 4  1 1 1
+4 4 4  1 1 1
+4 4 4  1 1 1
+
+top    bottom
+3 J 3  2 D 2
+3 K 3  2 E 2
+3 L 3  2 F 2
+
+->
+
+front  back
+5 J 5  0 D 0 
+5 K 5  0 E 0
+5 L 5  0 F 0
+
+left   right
+4 4 4  1 1 1
+4 4 4  1 1 1
+4 4 4  1 1 1
+
+top    bottom
+3 A 3  2 G 2  
+3 B 3  2 H 2
+3 C 3  2 I 2
+-------------
+
+Mv'
+
+front  back
+5 A 5  0 G 0
+5 B 5  0 H 0
+5 C 5  0 I 0
+
+left   right
+4 4 4  1 1 1
+4 4 4  1 1 1
+4 4 4  1 1 1
+
+top    bottom
+3 J 3  2 D 2
+3 K 3  2 E 2
+3 L 3  2 F 2
+
+->
+
+front  back
+5 J 5  0 D 0 
+5 K 5  0 E 0
+5 L 5  0 F 0
+
+left   right
+4 4 4  1 1 1
+4 4 4  1 1 1
+4 4 4  1 1 1
+
+top    bottom
+3 G 3  2 A 2  
+3 H 3  2 B 2
+3 I 3  2 C 2
+-------------
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 front  back
 5 5 5  0 0 0

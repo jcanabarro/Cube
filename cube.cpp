ddf39100058e88e240801f75d7fd3b636fc6b8f5
Renan Tashiro <renantashiro@hotmail.com>
2017-06-16 11:53:30 -0300

    alterei logica do representacao do cubo

--- cube.cpp
+++ cube.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <cstring>
+#include <stdexcept>
+#include <array>
 
 #define YELLOW 0
 #define BLUE   1
@@ -8,256 +9,202 @@
 #define GREEN  4 
 #define WHITE  5
 
-// cw  = clockwise
-// acw = anti-clockwise
-// h   = horizontoal
-// v   = vertical
-class Rubik 
+template<std::size_t D> 
+class Rubik
 {
+	typedef std::array<std::array<int,D>,D> face;
+	
 	public:
 		Rubik();
-		void right_cw();
-		void right_acw();
-		void left_cw();
-		void left_acw();
-		void up_cw();
-		void up_acw();
-		void down_cw();
-		void down_acw();
-		void front_cw();
-		void front_acw();
-		void back_cw();
-		void back_acw();
-		void hmiddle_cw();
-		void hmiddle_acw();
-		void vmiddle_cw();
-		void vmiddle_acw();
-	private:	
-		int front[3][3], left[3][3], right[3][3];
-		int back[3][3], top[3][3], bottom[3][3];
+		bool solved();
+		bool operator==(const Rubik &other) const;
+		void move_cw(int x, int y, int z, bool clockwise);
+		
+	private:
+		inline void swap_column(Rubik::face &a, Rubik::face &b, std::size_t index);
+		inline void rotate_face(Rubik::face &f, bool clockwise);
+		
+		const Rubik::face sfront, sright, stop;
+		const Rubik::face sback, sleft, sbottom;
+		Rubik::face front, right, top;
+		Rubik::face back, left, bottom;
+		std::size_t dim;
 };
 
-Rubik::Rubik()
+//--------- Métodos publicos ---------//
+
+template<std::size_t D>
+Rubik<D>::Rubik()
 {
-	memset(back, 0 + 9, sizeof back);
-	memset(right, 1, sizeof right);
-	memset(bottom, 2, sizeof bottom);
-	memset(top, 3, sizeof top);
-	memset(left, 4, sizeof left);
-	memset(front, 5, sizeof front);
+	back.fill(YELLOW);
+	sback.fill(YELLOW);
+	right.fill(BLUE);
+	sright.fill(BLUE);
+	bottom.fill(RED);
+	sbottom.fill(RED);
+	top.fill(ORANGE);
+	stop.fill(ORANGE);
+	left.fill(GREEN);
+	sleft.fill(GREEN);
+	front.fill(WHITE);
+	sfront.fill(WHITE);
+	dim = D;
 }
 
-// Falta checar da documentacao e no código
-void Rubik::right_cw()
+template<std::size_t D>
+bool Rubik<D>::solved()
 {
-	// back to bottom
-	std::swap(back[0][2], bottom[2][2]);
-	std::swap(back[1][2], bottom[1][2]);
-	std::swap(back[2][2], bottom[0][2]);
-	
-	// bottom to front
-	std::swap(bottom[0][2], front[0][2]);
-	std::swap(bottom[1][2], front[1][2]);
-	std::swap(bottom[2][2], front[2][2]);
-	
-	// front to top
-	std::swap(front[0][3], top[2][3]);
-	std::swap(front[1][3], top[1][3]);
-	std::swap(front[2][3], top[0][3]);
-	
-	// top to back
-	std::swap(top[0][3], back[0][3]);
-	std::swap(top[1][3], back[1][3]);
-	std::swap(top[2][3], back[2][3]);
-	
-	// right face
-	std::swap(right[0][0], right[0][2]);
-	std::swap(right[0][1], right[1][2]);
-	std::swap(right[0][2], right[2][2]);
-	std::swap(right[1][2], right[2][1]);
-	std::swap(right[2][2], right[2][0]);
-	std::swap(right[2][1], right[1][0]);
-	std::swap(right[2][0], right[0][0]);
-	std::swap(right[1][0], right[0][1]);
+	return front == sfront && back   == sback 
+	    && right == sright && left   == sleft 
+	    && top   == stop   && bottom == sbottom;
 }
 
-// Falta checar da documentacao e no código
-void Rubik::right_cw()
+template<std::size_t D>
+bool Rubik<D>::operator==(const Rubik &other) const
 {
-	// back to bottom
-	std::swap(back[0][2], bottom[2][2]);
-	std::swap(back[1][2], bottom[1][2]);
-	std::swap(back[2][2], bottom[0][2]);
+	return front == other.front && back   == other.back 
+	    && right == other.right && left   == other.left
+	    && top   == other.top   && bottom == other.bottom;
+}
+
+/**
+ *	x: Se diferente de -1, então implica em uma rotação em uma linha.
+ *     Seu valor indica qual linha será rotacionada. 
+ * 	y: Se diferente de -1, então implica em uma rotação em uma coluna.
+ *     Seu valor indica qual coluna será rotacionada.
+ *  z: Se diferente de -1, então implica em uma rotação em uma lateral.
+ *     Seu valor indica qual lateral será rotacionada.
+ *  clockwise: indica se a rotação será no sentido horário caso verda-
+ *             deiro, anti-horário caso contrário.
+ * 	Obs: 1. Todos os movimentos usam como referência a face 'front';
+ *       2. Não pode ter dois valores diferentes de -1.
+ */
+template<std::size_t D>
+void Rubik<D>::move_cw(int x, int y, int z, bool clockwise)
+{
+	// indices invalidos
+	if(x >= dim || y >= dim || z >= dim)
+	{
+		throw std::out_of_range();
+	}
+
+    // rotaciona uma face dependendo dos indice 
+	if(x == 0)
+	{
+		rotate_face(top, clockwise);
+	}
+	else if(x == dim - 1)
+	{
+		rotate_face(bottom, !clockwise);
+	}
+	else if(y == 0)
+	{
+		rotate_face(left, clockwise);
+	}
+	else if(y == dim - 1)
+	{
+		rotate_face(right, !clockwise);
+	}
+	else if(z == 0)
+	{
+		rotate_face(front, clockwise);
+	}
+	else if(z == dim - 1)
+	{
+		rotate_face(back, !clockwise);
+	}					
 	
-	// bottom to front
-	std::swap(bottom[0][2], front[0][2]);
-	std::swap(bottom[1][2], front[1][2]);
-	std::swap(bottom[2][2], front[2][2]);
-	
-	// front to top
-	std::swap(front[0][3], top[2][3]);
-	std::swap(front[1][3], top[1][3]);
-	std::swap(front[2][3], top[0][3]);
-	
-	// top to back
-	std::swap(top[0][3], back[0][3]);
-	std::swap(top[1][3], back[1][3]);
-	std::swap(top[2][3], back[2][3]);
-	
-	// right face
-	std::swap(right[0][0], right[0][2]);
-	std::swap(right[0][1], right[1][2]);
-	std::swap(right[0][2], right[2][2]);
-	std::swap(right[1][2], right[2][1]);
-	std::swap(right[2][2], right[2][0]);
-	std::swap(right[2][1], right[1][0]);
-	std::swap(right[2][0], right[0][0]);
-	std::swap(right[1][0], right[0][1]);
+	// rotaciona uma linha, coluna ou lateral
+	if(clockwise)
+	{
+		if(x != -1)
+	 	{
+			swap(front[x], right[x]);
+			swap(front[x], back[x]);
+			swap(front[x], left[x]);
+		}
+		else if(y != -1)
+		{
+			swap_column(front, top, y);
+			swap_column(front, back, y);
+			swap_column(front, bottom, y);
+		}
+		else if(z != -1)
+		{	
+			swap_column(right, top, z);
+			swap_column(right, left, z);
+			swap_column(right, bottom, z);
+		}
+	}
+	else
+	{
+		if(x != -1)
+		{
+			swap(front[x], left[x]);
+			swap(front[x], back[x]);
+			swap(front[x], right[x]);
+		}
+		else if(y != -1)
+		{
+			swap_column(front, bottom, y);
+			swap_column(front, back, y);
+			swap_column(front, top, y);
+		}
+		else if(z != -1)
+		{
+			swap_column(right, bottom, z);
+			swap_column(right, left, z);
+			swap_column(right, top, z);			
+		}		
+	}
+}
+
+//--------- Métodos privados ---------//
+
+template<std::size_t D>
+inline void Rubik<D>::rotate_face(Rubik::face &f, bool clockwise)
+{
+	if(clockwise)
+	{
+		for(int i = 0; i < dim / 2; i++)
+		{
+			for(int j = 0; j < dim - i - 1; j++)
+			{
+				int tmp = f[i][j];
+				
+				f[i][j] = f[dim - j - 1][i];
+				f[dim - j - 1][i] = f[dim - i - 1][dim - j - 1];
+				f[dim - i - 1][dim - j - 1] = f[j][dim - i - 1];
+				f[j][dim - i - 1] = tmp;
+			}
+		}
+	}
+	else
+	{
+		for(std::size_t i = 0; i < dim / 2; i++)
+		{
+			for(std::size_t j = 0; j < dim - i - 1; j++)
+			{
+				int tmp = f[i][j];
+				
+				f[i][j] = f[j][dim - i - 1];
+				f[j][dim- i - 1] = f[dim - i - 1][dim - j - 1];
+				f[dim - i - 1][dim - j - 1] = f[dim - j - 1][i];
+				f[dim - j - 1][i] = tmp;
+			}
+		}	
+	}
+}
+
+template<std::size_t D>
+inline void Rubik<D>::swap_column(Rubik::face &a, Rubik::face &b, std::size_t index)
+{
+	for(int i = 0; i < dim; i++)
+	{
+		swap(a[i][index], b[i][index]);
+	}
 }
 
 int main()
-{
-	
+{	
 }
-/*
---------------------------------------------------------------------
-         R              R'                 L               L'
-                                                     
-   front  back     front  back       front  back     front  back
-   5 5 5  0 0 0    5 5 5  0 0 0      5 5 5  0 0 0    5 5 5  0 0 0
-   5 5 5  0 0 0    5 5 5  0 0 0      5 5 5  0 0 0    5 5 5  0 0 0
-   5 5 5  0 0 0    5 5 5  0 0 0      5 5 5  0 0 0    5 5 5  0 0 0
-                                                     
-   left   right    left   right      left   right    left   right
-   4 4 4  A B C    4 4 4  A B C      A B C  1 1 1    A B C  1 1 1
-   4 4 4  H 1 D    4 4 4  H 1 D      H 4 D  1 1 1    H 4 D  1 1 1
-   4 4 4  G F E    4 4 4  G F E      G F E  1 1 1    G F E  1 1 1
-                                                     
-   top    bottom   top    bottom     top    bottom   top    bottom
-   3 3 3  2 2 2    3 3 3  2 2 2      3 3 3  2 2 2    3 3 3  2 2 2
-   3 3 3  2 2 2    3 3 3  2 2 2      3 3 3  2 2 2    3 3 3  2 2 2
-   3 3 3  2 2 2    3 3 3  2 2 2      3 3 3  2 2 2    3 3 3  2 2 2
-                                                     
-                                 to  
-                                                     
-   front  back     front  back       front  back     front  back
-   5 5 2  0 0 3    5 5 3  0 0 2      3 5 5  2 0 0    2 5 5  3 0 0
-   5 5 2  0 0 3    5 5 3  0 0 2      3 5 5  2 0 0    2 5 5  3 0 0
-   5 5 2  0 0 3    5 5 3  0 0 2      3 5 5  2 0 0    2 5 5  3 0 0
-                                                     
-   left   right    left   right      left   right    left   right
-   4 4 4  G H A    4 4 4  C D E      C D E  1 1 1    G H A  1 1 1
-   4 4 4  F 1 B    4 4 4  B 1 F      B 4 F  1 1 1    F 4 B  1 1 1
-   4 4 4  E D C    4 4 4  A H G      A H G  1 1 1    E D C  1 1 1
-                                                     
-   top    bottom   top    bottom     top    bottom   top    bottom
-   3 3 5  2 2 0    3 3 0  2 2 5      0 3 3  5 2 2    5 3 3  0 2 2
-   3 3 5  2 2 0    3 3 0  2 2 5      0 3 3  5 2 2    5 3 3  0 2 2
-   3 3 5  2 2 0    3 3 0  2 2 5      0 3 3  5 2 2    5 3 3  0 2 2
-   
-   
-         U                U'               D                D'
-                                                       
-   front  back     front  back       front  back     front  back
-   5 5 5  0 0 0    5 5 5  0 0 0      5 5 5  0 0 0    5 5 5  0 0 0
-   5 5 5  0 0 0    5 5 5  0 0 0      5 5 5  0 0 0    5 5 5  0 0 0
-   5 5 5  0 0 0    5 5 5  0 0 0      5 5 5  0 0 0    5 5 5  0 0 0
-                                                     
-   left   right    left   right      left   right    left   right
-   4 4 4  1 1 1    4 4 4  1 1 1      4 4 4  1 1 1    4 4 4  1 1 1
-   4 4 4  1 1 1    4 4 4  1 1 1      4 4 4  1 1 1    4 4 4  1 1 1
-   4 4 4  1 1 1    4 4 4  1 1 1      4 4 4  1 1 1    4 4 4  1 1 1
-                                                     
-   top    bottom   top    bottom     top    bottom   top    bottom
-   A B C  2 2 2    A B C  2 2 2      3 3 3  A B C    3 3 3  A B C
-   H 3 D  2 2 2    H 3 D  2 2 2      3 3 3  H 2 D    3 3 3  H 2 D
-   G F E  2 2 2    G F E  2 2 2      3 3 3  G F E    3 3 3  G F E
-                                                     
-                                to   
-                                                     
-   front  back     front  back       front  back     front  back
-   1 1 1  4 4 4    4 4 4  1 1 1      5 5 5  0 0 0    5 5 5  0 0 0
-   5 5 5  0 0 0    5 5 5  0 0 0      5 5 5  0 0 0    5 5 5  0 0 0
-   5 5 5  0 0 0    5 5 5  0 0 0      4 4 4  1 1 1    1 1 1  4 4 4
-                                                     
-   left   right    left   right      left   right    left   right
-   5 5 5  0 0 0    0 0 0  5 5 5      4 4 4  1 1 1    4 4 4  1 1 1
-   4 4 4  1 1 1    4 4 4  1 1 1      4 4 4  1 1 1    4 4 4  1 1 1
-   4 4 4  1 1 1    4 4 4  1 1 1      0 0 0  5 5 5    5 5 5  0 0 0
-                                                     
-   top    bottom   top    bottom     top    bottom   top    bottom
-   G H A  2 2 2    C D E  2 2 2      3 3 3  G H A    3 3 3  C D E 
-   F 3 B  2 2 2    B 3 F  2 2 2      3 3 3  F 2 B    3 3 3  B 2 F
-   E D C  2 2 2    A H G  2 2 2      3 3 3  E D C    3 3 3  A H G
-   
-   
-         F               F'               B               B'
-                                                     
-   front  back     front  back       front  back     front  back
-   A B C  0 0 0    A B C  0 0 0      5 5 5  0 0 0    5 5 5  0 0 0
-   H 5 D  0 0 0    H 5 D  0 0 0      5 5 5  0 0 0    5 5 5  0 0 0
-   G F E  0 0 0    G F E  0 0 0      5 5 5  0 0 0    5 5 5  0 0 0
-                                                     
-   left   right    left   right      left   right    left   right
-   4 4 4  1 1 1    4 4 4  1 1 1      4 4 4  1 1 1    4 4 4  1 1 1
-   4 4 4  1 1 1    4 4 4  1 1 1      4 4 4  1 1 1    4 4 4  1 1 1
-   4 4 4  1 1 1    4 4 4  1 1 1      4 4 4  1 1 1    4 4 4  1 1 1
-                                                     
-   top    bottom   top    bottom     top    bottom   top    bottom
-   3 3 3  2 2 2    3 3 3  2 2 2      3 3 3  A B C    3 3 3  A B C
-   3 3 3  2 2 2    3 3 3  2 2 2      3 3 3  H 2 D    3 3 3  H 2 D
-   3 3 3  2 2 2    3 3 3  2 2 2      3 3 3  G F E    3 3 3  G F E
-                                                      
-                                to   
-                                                      
-   front  back     front  back       front  back      front  back
-   G H A  0 0 0    C D E  0 0 0      5 5 5  C D E     5 5 5  G H A 
-   F 5 B  0 0 0    B 5 F  0 0 0      5 5 5  B 0 F     5 5 5  F 0 B
-   E D C  0 0 0    A H G  0 0 0      5 5 5  A H G     5 5 5  E D C
-                                                      
-   left   right    left   right      left   right     left   right
-   4 4 1  3 1 1    4 4 3  2 1 1      4 4 4  1 1 1     4 4 4  1 1 1
-   4 4 1  3 1 1    4 4 3  2 1 1      4 4 4  1 1 1     4 4 4  1 1 1
-   4 4 1  3 1 1    4 4 3  2 1 1      3 3 3  2 2 2     2 2 2  3 3 3
-                                                      
-   top    bottom   top    bottom     top    bottom    top    bottom
-   3 3 3  1 1 1    3 3 3  4 4 4      3 3 3  2 2 2     3 3 3  2 2 2 
-   3 3 3  2 2 2    3 3 3  2 2 2      3 3 3  2 2 2     3 3 3  2 2 2
-   4 4 4  2 2 2    1 1 1  2 2 2      1 1 1  4 4 4     4 4 4  1 1 1
-   
-   
-        Mh               Mh'              Mv               Mv'
-                                                      
-   front  back     front  back       front  back      front  back
-   5 5 5  0 0 0    5 5 5  0 0 0      5 A 5  0 G 0     5 A 5  0 G 0
-   A B C  G H I    A B C  G H I      5 B 5  0 H 0     5 B 5  0 H 0
-   5 5 5  0 0 0    5 5 5  0 0 0      5 C 5  0 I 0     5 C 5  0 I 0
-                                                      
-   left   right    left   right      left   right     left   right
-   4 4 4  1 1 1    4 4 4  1 1 1      4 4 4  1 1 1     4 4 4  1 1 1
-   L K J  D E F    L K J  D E F      4 4 4  1 1 1     4 4 4  1 1 1
-   4 4 4  1 1 1    4 4 4  1 1 1      4 4 4  1 1 1     4 4 4  1 1 1
-                                                      
-   top    bottom   top    bottom     top    bottom    top    bottom
-   3 3 3  2 2 2    3 3 3  2 2 2      3 J 3  2 D 2     3 J 3  2 D 2
-   3 3 3  2 2 2    3 3 3  2 2 2      3 K 3  2 E 2     3 K 3  2 E 2
-   3 3 3  2 2 2    3 3 3  2 2 2      3 L 3  2 F 2     3 L 3  2 F 2
-                                                      
-                                 to  
-                                                      
-   front  back     front  back       front  back      front  back
-   5 5 5  0 0 0    5 5 5  0 0 0      5 J 5  0 D 0     5 J 5  0 D 0 
-   J K L  D E F    L K J  D E F      5 K 5  0 E 0     5 K 5  0 E 0
-   5 5 5  0 0 0    5 5 5  0 0 0      5 L 5  0 F 0     5 L 5  0 F 0
-                                                      
-   left   right    left   right      left   right     left   right
-   4 4 4  1 1 1    4 4 4  1 1 1      4 4 4  1 1 1     4 4 4  1 1 1
-   I H G  A B C    C B A  G H FI     4 4 4  1 1 1     4 4 4  1 1 1
-   4 4 4  1 1 1    4 4 4  1 1 1      4 4 4  1 1 1     4 4 4  1 1 1
-                                                      
-   top    bottom   top    bottom     top    bottom    top    bottom
-   3 3 3  2 2 2    3 3 3  2 2 2      3 A 3  2 G 2     3 G 3  2 A 2 
-   3 3 3  2 2 2    3 3 3  2 2 2      3 B 3  2 H 2     3 H 3  2 B 2
-   3 3 3  2 2 2    3 3 3  2 2 2      3 C 3  2 I 2     3 I 3  2 C 2
---------------------------------------------------------------------
-*/

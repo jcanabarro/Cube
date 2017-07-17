c88bc79ca6ceda16af880853803b0e8986b60f39
Renan Tashiro <renantashiro@hotmail.com>
2017-07-17 14:18:14 -0300

    finish the job

--- heuristicsearch.cpp
+++ heuristicsearch.cpp
@@ -10,13 +10,6 @@ typedef std::pair<int,int> ii;
 #define COST first.first
 #define LAYER first.second
 
-//~ #define DEBUG
-#ifdef DEBUG
-#define PRINT(a) (std::cout << "DEFINE_TEST " << a << std::endl)
-#else
-#define PRINT(a)
-#endif
-
 template<std::size_t D>
 int _heuristic(std::array<std::array<char,D>,D> f, int color)
 {
@@ -76,7 +69,8 @@ bool astar(Rubik<D> cube)
 			v.move(move.get_mx()[i], move.get_my()[i], move.get_mz()[i], move.get_mcw()[i]);
 			
 			if(v.solved())  {
-				std::cout << state << std::endl;
+				std::cout << "A*=" << state << std::endl;
+				//~ v.print();
 				return true;
 			}
 			
@@ -116,20 +110,15 @@ int _layer(std::array<std::array<char,D>,D> f, int color, int layer)
 template<std::size_t D> 
 int layer(Rubik<D> cube, int layer_)
 {	
-	//~ int aux = _layer<D>(cube.front, WHITE, layer_) + _layer<D>(cube.back, YELLOW, layer_) + _layer<D>(cube.right, BLUE, layer_) + _layer<D>(cube.left, GREEN, layer_) + _layer<D>(cube.bottom, RED, D - 1) + (layer_ == D - 1) ? _layer<D>(cube.top, ORANGE, layer_) : 0;
 	int auxfront = _layer<D>(cube.front, WHITE, layer_) ;
 	int auxback = _layer<D>(cube.back, YELLOW, layer_) ;
 	int auxrig =  _layer<D>(cube.right, BLUE, layer_); 
 	int auxleft = _layer<D>(cube.left, GREEN, layer_); 
 	int auxbot =  _layer<D>(cube.bottom, RED, D - 1);
 	int auxtop = _layer<D>(cube.top, ORANGE, layer_) + (layer_ == D - 1) ? _layer<D>(cube.top, ORANGE, layer_) : 0;
-	int aux2 = auxfront + auxback + auxrig + auxleft + auxbot + auxtop;
+	int aux = auxfront + auxback + auxrig + auxleft + auxbot + auxtop;
 	
-	if (aux2 == 0){ 
-			cube.print();
-	}
-	return aux2;
-
+	return aux;
 }
 
 template<std::size_t D>
@@ -154,9 +143,6 @@ bool lastar(Rubik<D> cube)
 				
 		if(u.LAYER < layer_) continue;		
 				
-		//~ std::cout << "layer=" << u.LAYER << std::endl;		
-		//~ std::cout << "cost=" << u.COST << " states=" << state << std::endl;		
-				
 		state++;		
 		
 		for(std::size_t i = 0; i < 6 * D; i++)
@@ -165,33 +151,26 @@ bool lastar(Rubik<D> cube)
 			
 			v.move(move.get_mx()[i], move.get_my()[i], move.get_mz()[i], move.get_mcw()[i]);
 			
-			if(v.solved())  {
-				std::cout << state << std::endl;
-				return true;
-			}
-			
 			if(visited.find(v) == visited.end())
 			{
 				int hcost = layer<D>(v, layer_);
 				
 				if(hcost == 0)
 				{
-					layer_ = std::max(layer_, u.LAYER + 1);
-					
-					if(u.LAYER < layer_)
-					{
-						std::cout << "STATE=" << state << std::endl;
+					if(layer_ < u.LAYER + 1)
+						std::cout << "STATE=" << state << " LAYER=" << u.LAYER+1 << std::endl; 
 						
-						if(layer_ == 1){
-							std::cout << "Termino Primeira camada" << std::endl;
-							return true;
-						}
-					}
-					
+					layer_ = std::max(layer_, u.LAYER + 1);
 				}
 				
 				next.push({{1 + u.COST + hcost, layer_}, Rubik<D>(v)});
 				visited.insert(Rubik<D>(v));
+			}
+			
+			if(v.solved())  {
+				std::cout << "LA*=" << state << std::endl;
+				//~ v.print();
+				return true;
 			}
 		}
 	}
@@ -201,10 +180,8 @@ bool lastar(Rubik<D> cube)
 
 int main()
 {	
-	PRINT(4);
-	Shuffle<3> shuffle;
-	Rubik<3> cube = shuffle.random(5); // 3x3 Com quinze ainda não dá com 15
-	astar<3>(cube);
-	cube.print();
-	lastar<3>(cube);
+	Shuffle<2> shuffle;
+	Rubik<2> cube = shuffle.random(12); // 3x3 Com quinze ainda não dá com 15
+	lastar<2>(cube);
+	astar<2>(cube);
 }
